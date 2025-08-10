bcf49aa58c8ef6200eef04a63cb312aab6950b18
sanjitsingha <itsmesanjitsingh@gmail.com>
2025-08-10 22:44:20 +0530

    refee

--- src/app/_components/SearchModel.jsx
+++ src/app/_components/SearchModel.jsx
@@ -1,6 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { LocationEdit } from "lucide-react";
+import {
+  Car,
+  Cctv,
+  ChartColumnBigIcon,
+  LocationEdit,
+  SmartphoneCharging,
+} from "lucide-react";
 import { getDistanceKm } from "../../utils/distance";
 import { useRouter } from "next/navigation";
 import { useParkingStore } from "../store/useParkingStore";
@@ -76,53 +82,48 @@ const CurrentLocationParking = () => {
 
   return (
     <div className="md:w-[400px] w-full h-fit p-2 bg-gray-200">
-      {/* Current Location Button */}
-      <div className="w-full mt-2 bg-green-400 rounded-md h-[60px]">
-        <button
-          onClick={handleGetCurrentLocation}
-          className="w-full justify-center cursor-pointer flex items-center gap-2 h-full"
-        >
-          Get Current Location
-          <LocationEdit size={20} />
-        </button>
-      </div>
-
-      {/* Show Coords */}
-      {selectedCoords && (
-        <p className="mt-2 text-sm text-gray-600">
-          Your Location: {selectedCoords.lat.toFixed(4)},{" "}
-          {selectedCoords.lon.toFixed(4)}
-        </p>
-      )}
-
       {/* Nearby Parking Lots */}
       {nearbyLots.length > 0 && (
         <div className="mt-4 bg-white rounded-md p-2">
-          <h3 className="font-bold mb-2">Nearby Parking Lots (within 5km)</h3>
           {nearbyLots.map((lot) => (
             <div
               onClick={() => handleSelectLot(lot)}
               key={lot.id}
-              className="border-b py-1 cursor-pointer hover:bg-gray-100"
+              className=" p-2 cursor-pointer hover:bg-gray-100"
             >
-              <div className="flex justify-between items-center">
-                <div>
-                  <p className="font-medium">{lot.name}</p>
-                  <p className="text-xs text-gray-500">
-                    {lot.category} • ₹{lot.price_per_hour}/hr
+              <div className="w-full flex ">
+                <span className="flex-1">
+                  <p className="text-xl font-medium">{lot.name}</p>
+                </span>
+                <span className="bg-green-400/50 px-1 py-0.5 rounded-sm">
+                  <p>
+                    {lot.price_per_hour}
+                    {"/hr"}
                   </p>
-                  <p className="text-xs text-gray-400">{lot.status}</p>
-                </div>
-                {lot.ev_charging && (
-                  <span className="bg-green-200 text-green-800 text-xs px-2 py-1 rounded">
-                    EV
-                  </span>
-                )}
-                {lot.cctv && (
-                  <span className="bg-blue-200 text-blue-800 text-xs px-2 py-1 rounded">
-                    CCTV
-                  </span>
-                )}
+                </span>
+              </div>
+              <div className="w-full mt-3 flex flex-col gap-2">
+                <span className="flex items-center gap-2 ">
+                  <Car size={32} />
+                  <p>{lot.category}</p>
+                </span>
+                <span className="flex items-center gap-2 ">
+                  <Cctv size={32} />
+                  <p>{lot.cctv && "CCTV Available"}</p>
+                </span>
+                <span className="flex items-center gap-2">
+                  {lot.ev_charging === 1 ? (
+                    <>
+                      <SmartphoneCharging size={32} />
+                      <span>EV Charging Available</span>
+                    </>
+                  ) : (
+                    <>
+                      <SmartphoneCharging size={32} />
+                      <span>Not Available</span>
+                    </>
+                  )}
+                </span>
               </div>
             </div>
           ))}
