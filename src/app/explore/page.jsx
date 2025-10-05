7f185577198ba75cbbc97ecdb09528f63e90fd5e
sanjitsingha <itsmesanjitsingh@gmail.com>
2025-10-05 11:17:18 +0530

    up

--- src/app/explore/page.jsx
+++ src/app/explore/page.jsx
@@ -11,9 +11,28 @@ const page = () => {
 
   return (
     <div className="font-inter">
-      <div className="w-full h-[calc(100vh-90px)] ">
+      <div className="w-full h-[calc(100vh-90px)]">
+        <div className="w-full  p-4 bg-[#18191F]  ">
+          <div className="w-full">
+            <h1 className=" text-3xl font-semibold leading-11 text-white">
+              <span className="text-lg text-yellow-400">Where ever you go</span>{" "}
+              <br />
+              We are available!
+            </h1>
+          </div>
+          <div className="w-full flex gap-2 mt-4">
+            <input
+              className="bg-white w-[80%] border-yellow-400 border outline-none px-3 h-[46px] rounded-md"
+              type="text"
+              placeholder="Search for a Location?"
+            />
+            <button className="h-[46px] rounded-md px-4  bg-yellow-400">
+              Go
+            </button>
+          </div>
+        </div>
         {/* Location permision */}
-        <div className="w-full hidden bg-[#24262e] p-4 pt-50  flex-col items-center h-full text-center gap-6">
+        <div className="w-full hidden bg-[#24262e] p-4   flex-col items-center h-full text-center gap-6">
           <p className="font-semibold text-white text-xl">Enable Location</p>
           <div className="bg-[#18191F] p-6 rounded-full">
             <Navigation className="text-white mx-auto" size={60} />
@@ -27,7 +46,6 @@ const page = () => {
           </button>
         </div>
         <CurrentLocationParking />
-        {/* <AvailableSpaces /> */}
       </div>
     </div>
   );
